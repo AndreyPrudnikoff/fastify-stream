07a6746200055a1f0f98ec4d55187251e607634c
= <=>
2021-11-24 00:00:09 +0200

    routes declarative

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path')
 const client = require("./db")
 const fs = require("fs")
 
+const routes = require('./routes')
+
 const images = path.resolve(__dirname, './static')
 if (!fs.existsSync(images)) fs.mkdirSync(images)
 
@@ -10,13 +12,9 @@ fastify.register(require('fastify-file-upload'))
 fastify.register(require('fastify-websocket'))
 fastify.register(require('fastify-jwt'), {secret: 'token'})
 fastify.register(require('fastify-static'), {root: path.join(__dirname, '/static'), prefix: '/static/'})
-fastify.register(require('fastify-cors'), {origin: ['http://localhost:3000', 'http://192.168.1.8:3000']})
+fastify.register(require('fastify-cors'), {origin: ['http://localhost:8000', 'http://localhost:3000', 'http://192.168.1.6:3000', 'http://192.168.1.10:3000']})
 
-fastify.register(require('./routes/auth'))
-fastify.register(require('./routes/users'))
-fastify.register(require('./routes/videos'))
-fastify.register(require('./routes/files'))
-fastify.register(require('./routes/ws'))
+routes.forEach(file => fastify.register(require(file)))
 
 fastify.decorate("authenticate", async function (request, reply) {
     try {
@@ -34,16 +32,11 @@ fastify.decorate("authenticate", async function (request, reply) {
     }
 })
 
-// fastify.get('/', async (request, reply) => {
-//     return reply.sendFile('index.html')
-// })
-
-
 const start = async () => {
 
     try {
         await client.connect()
-        await fastify.listen(5000)
+        await fastify.listen(5000, "0.0.0.0")
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
